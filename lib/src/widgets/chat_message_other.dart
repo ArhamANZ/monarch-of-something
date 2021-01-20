815bc275df6b326a43b8c5924de9c7f7ee615f47
Arham Zahedy <arhamanz152@gmail.com>
2021-01-20 12:06:34 +0600

    Test Commit

--- lib/src/widgets/chat_message_other.dart
+++ lib/src/widgets/chat_message_other.dart
@@ -3,56 +3,53 @@ import 'package:flutter/material.dart';
 class ChatMessageOther extends StatelessWidget {
   final int index;
   final Map<String, dynamic> data;
-  final bool showAvatar; 
+  final bool showAvatar;
 
   const ChatMessageOther(
-    {Key key, this.index, this.data, this.showAvatar = true}) 
-    : super(key: key);
-
+      {Key key, this.index, this.data, this.showAvatar = true})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(
-        horizontal: 10,
-        vertical: 6
-      ),
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-        if (showAvatar)
-          CircleAvatar(
-            backgroundImage: NetworkImage(data['photo_url']),
-          )
-        else 
-          SizedBox(width: 40),
-        SizedBox(width: 10),
-        Container(
-          constraints: BoxConstraints(
-            maxWidth: 300,
+          if (showAvatar)
+            CircleAvatar(
+              backgroundImage: NetworkImage(data['photo_url']),
+            )
+          else
+            SizedBox(width: 40),
+          SizedBox(width: 10),
+          Container(
+            constraints: BoxConstraints(
+              maxWidth: 300,
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  '${data['author']} said:',
+                  style: TextStyle(
+                    color: Colors.blueGrey,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 11,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(data['value']),
+              ],
+            ),
           ),
-          padding: const EdgeInsets.symmetric(
-            horizontal: 10,
-            vertical: 12
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(5),
-          ),
-          child: Column(
-            children: [
-              Text('${data['author']} said:', style: TextStyle(
-                color: Colors.blueGrey,
-                fontStyle: FontStyle.italic,
-                fontSize: 11,
-                fontWeight: FontWeight.bold,
-              ),),
-              SizedBox(height: 5),
-              Text(data['value']),
-            ],
-          ),
-        ),
-      ],),
+        ],
+      ),
     );
   }
 }
