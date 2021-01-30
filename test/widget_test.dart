13eacfb16c36b7b3289b3dd7301e52408eefc2fc
Arham Zahedy <arhamanz152@gmail.com>
2021-01-30 16:35:00 +0600

    Made some minor changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:monarchs_of_something/main.dart';
 import 'package:monarchs_of_something/src/main_screen.dart';
 
 void main() {
